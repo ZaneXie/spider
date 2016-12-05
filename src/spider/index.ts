60bb1396cafa04737e0023a2db5481f65f2577c8
xiezongjun <xiezjsc@gmail.com>
2016-12-05 17:35:31 +0800

    该用async/await

--- src/spider/index.ts
+++ src/spider/index.ts
@@ -10,30 +10,30 @@ import {getDebugger} from "../util/debug";
 let debug = getDebugger("spider");
 let csv: any = Bluebird.promisifyAll(require('csv'));
 
-function parse(pageNum: number) {
+async function parse(pageNum: number) {
     let url = "http://cd.lianjia.com/chengjiao/pg" + pageNum + "/";
     debug("parsing url: " + url);
-    return requestPromise(url).then(cheerio.load).then(($) => {
-        let result = [];
-        $(".main-box .clinch-list li").each(function (key, ele) {
-            let title = $(ele).find("h2 a").text();
-            let infos = $(ele).find(".div-cun");
-            let time = $(infos.get(0)).text();
-            let price = $(infos.get(1)).text();
-            let total = $(infos.get(2)).text();
-            let detail = $(ele).find(".other .con").text();
-            let url = $(ele).find(".info-panel h2 a").attr("href");
-            let pattern = /.*\/(.*)\.html/
-            let match = pattern.exec(url);
-            let id = null;
-            if (match && match[1]) {
-                id = match[1];
-            }
-            result.push({id, title, detail, time, price, total, url});
-        });
-        debug("get " + result.length + " elements.");
-        return result;
+    let html = await requestPromise(url);
+    let $ = cheerio.load(html);
+    let result = [];
+    $(".main-box .clinch-list li").each(function (key, ele) {
+        let title = $(ele).find("h2 a").text();
+        let infos = $(ele).find(".div-cun");
+        let time = $(infos.get(0)).text();
+        let price = $(infos.get(1)).text();
+        let total = $(infos.get(2)).text();
+        let detail = $(ele).find(".other .con").text();
+        let url = $(ele).find(".info-panel h2 a").attr("href");
+        let pattern = /.*\/(.*)\.html/
+        let match = pattern.exec(url);
+        let id = null;
+        if (match && match[1]) {
+            id = match[1];
+        }
+        result.push({id, title, detail, time, price, total, url});
     });
+    debug("get " + result.length + " elements.");
+    return result;
 }
 
 let result = '';
@@ -42,7 +42,7 @@ let i = 1;
 let totalPage = 10;
 let thread = 10;
 
-let run = () => {
+let run = async() => {
     if (i > totalPage) {
         debug("writing files...");
         fs.writeFileSync("D:\\123.csv", result, "utf-8");
@@ -55,7 +55,8 @@ let run = () => {
             result += line;
         }));
     }
-    Promise.all(jobs).then(run);
+    await Promise.all(jobs);
+    run();
 }
 
 run();
