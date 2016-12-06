21b60f382b8e10e3c8ed8f8fbebec10543776bc0
heqing90 <heqing_90@outlook.com>
2016-12-06 12:29:29 +0800

    添加基本解析功能

---
+++ src/spider/houseselling.ts
@@ -0,0 +1,45 @@
+/**
+ * Created by qing.he on 2016/12/6.
+ */
+
+import requestPromise = require("request-promise");
+import cheerio = require("cheerio");
+import {getDebugger} from "../util/debug";
+let debug = getDebugger("spider");
+
+export let parseHtml = (houseId: number) => parse(houseId)
+
+async function parse(houseId: number) {
+    let url = "http://cd.lianjia.com/ershoufang/c" + houseId + "/";
+    debug("parsing url: " + url);
+    let html = await requestPromise(url);
+    let $ = cheerio.load(html);
+    let result = [];
+    $(".main-box .house-lst li").each(function (key, ele) {
+        let title = $(ele).find("h2 a").text();
+        let where = $(ele).find(".where a span").text();
+
+        let info = $(ele).find(".where span");
+        let layout = $(info.get(0)).find("span").text();
+        let area = $(info.get(1)).find(("span")).text();
+
+        let location = $(ele).find(".other .con a").text();
+        let detail = $(ele).find(".other .con").text();
+
+        let totalPrice = $(ele).find(".price span").text();
+        let unitPrice = $(ele).find(".price-pre").text();
+
+        let visitorNum = $(ele).find(".square span").text();
+
+        let url = $(ele).find(".info-panel h2 a").attr("href");
+        let pattern = /.*\/(.*)\.html/
+        let match = pattern.exec(url);
+        let id = null;
+        if (match && match[1]) {
+            id = match[1];
+        }
+        result.push({id, title, detail, where, totalPrice, unitPrice, visitorNum, url});
+    });
+    debug("get " + result.length + " elements.");
+    return result;
+}
---
+++ src/spider/housesold.ts
@@ -0,0 +1,37 @@
+/**
+ * Created by qing.he on 2016/12/6.
+ */
+
+import requestPromise = require("request-promise");
+import cheerio = require("cheerio");
+import fs = require('fs');
+import {getDebugger} from "../util/debug";
+let debug = getDebugger("spider");
+
+export let parseHtml = (houseId: number) => parse(houseId)
+
+async function parse(houseId: number) {
+    let url = "http://cd.lianjia.com/chengjiao/c" + houseId + "/";
+    debug("parsing url: " + url);
+    let html = await requestPromise(url);
+    let $ = cheerio.load(html);
+    let result = [];
+    $(".main-box .clinch-list li").each(function (key, ele) {
+        let title = $(ele).find("h2 a").text();
+        let infos = $(ele).find(".div-cun");
+        let time = $(infos.get(0)).text();
+        let price = $(infos.get(1)).text();
+        let total = $(infos.get(2)).text();
+        let detail = $(ele).find(".other .con").text();
+        let url = $(ele).find(".info-panel h2 a").attr("href");
+        let pattern = /.*\/(.*)\.html/
+        let match = pattern.exec(url);
+        let id = null;
+        if (match && match[1]) {
+            id = match[1];
+        }
+        result.push({id, title, detail, time, price, total, url});
+    });
+    debug("get " + result.length + " elements.");
+    return result;
+}
